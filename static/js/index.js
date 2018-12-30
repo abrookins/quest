e09034170d333e00b14b71ab39ea6911990689f5
Andrew Brookins <a.m.brookins@gmail.com>
2018-12-30 08:51:50 -0800

    wip on react

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+function Welcome(props) {
+  return
+  <
+    h1> Hello, {props.name}
+    <
+    /h1>;
+    }
+
+    const element =
+    <
+      Welcome
+      name="world" / >;
+    ReactDOM.render(
+    element,
+    document.getElementById('react')
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './static/js/index',
+
+  output: {
+    path: path.resolve('./static/bundles/'),
+    filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+
+};
