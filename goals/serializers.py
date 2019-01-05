5f523b1d3518defa608d31dc6ba4fcf84e2c0aa1
Andrew Brookins <a.m.brookins@gmail.com>
2019-01-04 17:34:01 -0800

    Fix percentage complete

--- goals/serializers.py
+++ goals/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from goals.models import Goal
 
+
 class GoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goal
-        fields = ('id', 'name', 'description', 'slug')
+        fields = ('id', 'name', 'description', 'slug', 'percentage_complete')
