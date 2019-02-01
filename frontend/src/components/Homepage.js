232aa6d2e9d82e375e49adb870960d55187c7bd0
Andrew Brookins <a.m.brookins@gmail.com>
2019-02-01 06:31:42 -0800

    Hide recommended if nothing to recommend

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -6,7 +6,7 @@ import Utils from './Utils'
 import '../css/application.scss'
 
 class Homepage extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       goals: [],
@@ -17,15 +17,15 @@ class Homepage extends React.Component {
     this.handleStart = this.handleStart.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     Goals.getAll()
       .catch(() => {
-        this.setState({ placeholder: 'Something went wrong' })
+        this.setState({placeholder: 'Something went wrong'})
       })
-      .then(response => this.setState({ goals: response.data, loaded: true }))
+      .then(response => this.setState({goals: response.data, loaded: true}))
   }
 
-  handleDelete (id) {
+  handleDelete(id) {
     if (!window.confirm('Delete learning goal?')) {
       return
     }
@@ -49,7 +49,7 @@ class Homepage extends React.Component {
     })
   }
 
-  handleStart (id) {
+  handleStart(id) {
     Goals.start(id).then((request) => {
       let goals = this.state.goals.map((goal) => {
         return goal.id !== id ? goal : request.data
@@ -60,22 +60,31 @@ class Homepage extends React.Component {
     })
   }
 
-  render () {
+  render() {
     const yourGoals = this.state.goals.filter((goal) => goal.user_has_started || !goal.is_public)
     const recommendedGoals = this.state.goals.filter((goal) => goal.is_public && !goal.user_has_started)
+    let recommended = ""
+
+    if (recommendedGoals.length) {
+      recommended = <div className="learning-goals">
+        <h2 className="subtitle">Recommended Goals</h2>
+        <GoalsList goals={recommendedGoals} handleDelete={this.handleDelete}
+                   handleStart={this.handleStart}/>
+      </div>
+    }
+
     return <div className='your-goals'>
       <div className="learning-goals">
         <h2 className="subtitle">Your Learning Goals</h2>
-        <GoalsList moreUrl="/goals" goals={yourGoals} handleDelete={this.handleDelete} handleStart={this.handleStart} />
+        <GoalsList moreUrl="/goals" goals={yourGoals} handleDelete={this.handleDelete}
+                   handleStart={this.handleStart}/>
       </div>
-      <div className="learning-goals">
-        <h2 className="subtitle">Recommended Goals</h2>
-        <GoalsList goals={recommendedGoals} handleDelete={this.handleDelete} handleStart={this.handleStart} />
-      </div>
+
+      {recommended}
     </div>
   }
 }
 
 const wrapper = document.getElementById('homepage')
 
-wrapper ? ReactDOM.render(<Homepage />, wrapper) : null
+wrapper ? ReactDOM.render(<Homepage/>, wrapper) : null
