7c8f2754e71dff124091fed033a3da7b61249c78
Andrew Brookins <a.m.brookins@gmail.com>
2019-04-10 04:35:02 -0700

    checkpoint

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# tag::Account[]
 class Account(models.Model):
-    users = models.OneToOne(User, related_name)
+    name = models.CharField(max_length=255, help_text="Name of the account")
+# end::Account[]
+
+
+# tag::UserProfile[]
+class UserProfile(models.Model):
+    user = models.ForeignKey(
+        User,
+        help_text="The user to whom this profile belongs",
+        on_delete=models.CASCADE)
+    account = models.ForeignKey(  # <1>
+        Account,
+        help_text="The account to which this user belongs",
+        on_delete=models.DO_NOTHING)
+# end::UserProfile[]
